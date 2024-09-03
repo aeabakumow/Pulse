fd4af338ff872ef65c16e7baffbce5710eb7a253
Daniela <dtricolici12345@gmail.com>
2024-09-03 18:32:42 +0200

    modal windows added

--- src/js/script.js
+++ src/js/script.js
@@ -46,4 +46,20 @@ $(document).ready(function () {
   }
   toggleClass(".catalog-item__link");
   toggleClass(".catalog-item__back");
+
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  })
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  })
+  
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  })
 });
